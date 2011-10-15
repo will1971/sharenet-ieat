0d18f67bc95226488d8cc049b74bef42a48bd58f
santal.li@gmail.com <santal.li@gmail.com@e41172e0-e998-963f-eca5-9eaf1885020b>
2011-10-15 07:01:31 +0000

    添加点菜代理服务器

---
+++ branches/v3/ieat-server/webproxy/src/net/mygeda/ieat/Order.java
@@ -0,0 +1,28 @@
+package net.mygeda.ieat;
+
+public class Order {
+	private String id ;
+	private String count ;
+	
+	public String getCount() {
+		return count;
+	}
+
+	public void setCount(String count) {
+		this.count = count;
+	}
+
+	public Order(){
+	}
+	
+	// =================== bla bla ================
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	
+}
